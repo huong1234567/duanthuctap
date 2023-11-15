395025b9a2179b323eeccd181285af5ce0146e70
huong1234567 <huongdtpd06182@fpt.edu>
2023-11-15 18:19:57 +0700

    add quenmatkhau and trang chi tiet

---
+++ src/main/java/poly/edu/shop/controller/controller.java
@@ -0,0 +1,12 @@
+package poly.edu.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+    @RequestMapping("quenmatkhau")
+    public String qmk(){
+        return "quenmatkhau";
+    }
+}
